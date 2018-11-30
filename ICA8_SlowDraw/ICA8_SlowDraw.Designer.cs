126c770ffe7aa06f4d6e86fe2238881f9a353c19
rmak-naz <40699936+rmak-naz@users.noreply.github.com>
2018-11-29 18:53:27 -0700

    Add files via upload

---
+++ ICA8_SlowDraw/ICA8_SlowDraw.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ICA8_SlowDraw
+{
+    partial class ICA8_SlowDraw
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LB_DrawData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LB_DrawData
+            // 
+            this.LB_DrawData.AutoSize = true;
+            this.LB_DrawData.Location = new System.Drawing.Point(147, 9);
+            this.LB_DrawData.Name = "LB_DrawData";
+            this.LB_DrawData.Size = new System.Drawing.Size(190, 13);
+            this.LB_DrawData.TabIndex = 0;
+            this.LB_DrawData.Text = "segs in queue, estimated time to draw :";
+            // 
+            // ICA8_SlowDraw
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 47);
+            this.Controls.Add(this.LB_DrawData);
+            this.Name = "ICA8_SlowDraw";
+            this.Text = "Slow Draw";
+            this.Load += new System.EventHandler(this.ICA8_SlowDraw_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LB_DrawData;
+    }
+}
+
---
+++ ICA8_SlowDraw/ICA8_SlowDraw.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ICA8_SlowDraw
+{
+    public partial class ICA8_SlowDraw : Form
+    {
+        private CDrawer _canvas = new CDrawer();
+        private Queue<Point> _qPoints = new Queue<Point>();
+        private Point _lastKnownPoint;
+        private Stopwatch _sw = new Stopwatch();
+        private const double delay = 50;
+
+        public ICA8_SlowDraw()
+        {
+            InitializeComponent();            
+        }
+
+        private void ICA8_SlowDraw_MouseMove(Point pos, CDrawer dr)
+        {
+            lock (_qPoints)
+            {
+                _qPoints.Enqueue(pos);
+            }
+            Thread.Sleep(0);
+        }
+
+        private void ICA8_SlowDraw_Load(object sender, EventArgs e)
+        {
+            Thread drawThread = null;
+
+            _canvas.MouseMove += ICA8_SlowDraw_MouseMove;
+
+            drawThread = new Thread(new ThreadStart(Render));
+            drawThread.IsBackground = true;
+            drawThread.Start();
+        }
+
+        private void Render()
+        {
+            Queue<Point> queueCopy;
+            Point nextPos;
+
+            _sw.Start();
+            while (true)
+            {
+                while (_sw.ElapsedMilliseconds < delay) ;
+
+                lock (_qPoints)
+                {
+                    queueCopy = _qPoints;                    
+                }
+
+                Invoke(new Action(() => LB_DrawData.Text =
+                                            queueCopy.Count.ToString() +
+                                            " segs in queue, estimated time to draw : " +
+                                            (queueCopy.Count * delay / 1000).ToString("F1") +
+                                            " seconds."));
+
+                if (queueCopy.Count > 0)
+                {
+                    lock (_qPoints)
+                    {
+                        nextPos = _qPoints.Dequeue();
+                    }                    
+
+                    _canvas.AddLine(_lastKnownPoint.X, _lastKnownPoint.Y, nextPos.X, nextPos.Y, Color.Red);
+
+                    _lastKnownPoint = nextPos;
+                }
+
+                _sw.Restart();
+                //Thread.Sleep(50);
+
+
+
+            }
+        }
+    }
+}
